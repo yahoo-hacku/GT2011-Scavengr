63a7f1ba5c3cdf87d0fb9e01fe417ee0593fc4c7
Ben Olive <ben.olive@gatech.edu>
2011-03-11 17:47:10 -0500

    Quests Controller

---
+++ server/app/controllers/api/quests_controller.rb
@@ -0,0 +1,27 @@
+require 'uuidtools'
+
+module Api
+  class QuestsController < ApiController
+    def index
+      respond_with ""
+    end
+    def new
+      respond_with ""
+    end
+    def create
+      respond_with ""
+    end
+    def show
+      respond_with ""
+    end
+    def edit
+      respond_with ""
+    end
+    def update
+      respond_with ""
+    end
+    def destroy
+      respond_with ""
+    end
+  end
+end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -10,6 +10,7 @@ Huntr::Application.routes.draw do
   namespace 'api' do
     get "login" => 'users#login'
     get "logout" => 'users#logout'
+    resoucres :quests
   end
 
   # root :to => "welcome#index"
