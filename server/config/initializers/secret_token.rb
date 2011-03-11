5913e5bda58219bd4799d5104a8615fb68426afd
Ben Olive <ben.olive@gatech.edu>
2011-03-11 12:27:30 -0500

    Rails

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huntr::Application.config.secret_token = '02c28db33f1ebe9f4e573e3fa22e3eded530ae3bfd9fcd5461467707dc1ca984c05b1a1d8755d7bd354b4b8fe73c2946f956aca7965d011d2947d269b822601f'
