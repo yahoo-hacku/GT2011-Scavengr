4b32d8744121f474194e9700dacf7c97e88d72b8
Alex Milstead <alex@xanderal.com>
2011-03-11 17:44:58 -0500

    Basic login and create account structure fleshed out.

---
+++ app/src/net/moosen/huntr/results/Account.java
@@ -0,0 +1,10 @@
+package net.moosen.huntr.results;
+
+/**
+ * TODO: Enter class description.
+ */
+public interface Account
+{
+    public static final Integer ACCOUNT_CREATE_SUCCESS = 1;
+    public static final Integer ACCOUNT_CREATE_FAILED = 2;
+}
