e7e822c26da61c3015ceedf5d1f780fc51a3ad64
Ben Olive <ben.olive@gatech.edu>
2011-03-12 02:05:06 -0500

    Comments

---
+++ server/app/controllers/api/comments_controller.rb
@@ -0,0 +1,20 @@
+class Api::CommentsController < Api::ApplicationController
+  before_filter :setup_api, :except => [ :index ]
+
+  def index
+    @quest = @user.quests.find(params[:quest_id])
+    respond_with @quest.comments
+  end
+
+  private
+
+  def setup_api
+    @quest = @user.quests.find(params[:quest_id])
+    @active_params = params[:comment]
+    if params[:action] == 'create'
+      @active_object = @quest.comments.build
+    else
+      @active_object = @quest.comments.find(params[:id])
+    end
+  end
+end
