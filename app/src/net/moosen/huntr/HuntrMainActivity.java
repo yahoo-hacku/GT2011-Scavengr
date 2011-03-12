9bf2baeff8ab562115510ae7b6a0edc8729253ff
Ben Olive <ben.olive@gatech.edu>
2011-03-12 05:58:44 -0500

    Changes to Initial Activity do it work?

--- app/src/net/moosen/huntr/HuntrMainActivity.java
+++ app/src/net/moosen/huntr/HuntrMainActivity.java
@@ -1,5 +1,12 @@
 package net.moosen.huntr;
 
+import static net.moosen.huntr.api.ApiHandler.PREF_API_KEY;
+import static net.moosen.huntr.api.ApiHandler.PREF_USERNAME;
+import net.moosen.huntr.activities.HomeTabsActivity;
+import net.moosen.huntr.activities.account.AccountLoginActivity;
+import net.moosen.huntr.api.ApiHandler;
+import net.moosen.huntr.api.ApiHandler.API_ACTION;
+import android.R;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -8,135 +15,94 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.ProgressBar;
-import net.moosen.huntr.activities.HomeTabsActivity;
-import net.moosen.huntr.activities.account.AccountLoginActivity;
-import net.moosen.huntr.api.ApiHandler;
-import net.moosen.huntr.api.ApiHandler.API_ACTION;
-
-import static net.moosen.huntr.api.ApiHandler.PREF_API_KEY;
-import static net.moosen.huntr.api.ApiHandler.PREF_USERNAME;
+import android.widget.Toast;
 
 /**
  * TODO: Enter class description.
  */
-public class HuntrMainActivity extends Activity
-{
-    private static final int PROGRESS = 0x1;
+public class HuntrMainActivity extends Activity {
 
-    private ProgressBar progress;
+	private static final int PROGRESS = 0x1;
 
-    private int progress_status = 0;
+	private ProgressBar progress;
 
-    private Handler handler = new Handler();
+	private final int progress_status = 0;
 
-    private boolean ready = false;
+	private final Handler handler = new Handler();
 
-    private static enum PROGRESS_STATE
-    {
-        LOOP,
-        LOGIN,
-        HOME_TABS
-    }
+	private final boolean ready = false;
 
-    private PROGRESS_STATE progress_state = PROGRESS_STATE.LOOP;
+	private void handlePingResult(final boolean login_valid) {
+		runOnUiThread(new Runnable() {
 
-    @Override
-    protected void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
+			public void run() {
+				final Intent next = new Intent();
+				if (login_valid) {
+					next.setClass(HuntrMainActivity.this, HomeTabsActivity.class);
+				} else {
+					next.setClass(HuntrMainActivity.this, AccountLoginActivity.class);
+				}
+				startActivity(next);
+				finish();
+			}
+		});
+	}
 
-        ApiHandler.GetInstance().setPreferences(getPreferences(Context.MODE_PRIVATE));
-        setContentView(R.layout.main);
-        progress = (ProgressBar) findViewById(R.id.progress_main);
-        final Intent login = new Intent(), home_tabs = new Intent();
-        login.setClass(this, AccountLoginActivity.class);
-        home_tabs.setClass(this, HomeTabsActivity.class);
+	private void check_login() {
+		new Thread(new Runnable()
+		{
 
-        // Start lengthy operation in a background thread
-        new Thread(new Runnable()
-        {
-            public void run()
-            {
-                boolean die = false;
-                while (progress_status < 100 && !die)
-                {
-                    try { Thread.sleep(10); }
-                    catch (final InterruptedException ex) {}
+			public void run()
+			{
+				ApiHandler.GetInstance().setPreferences(getPreferences(Context.MODE_PRIVATE));
+				SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+				final String api_key = preferences.getString(PREF_API_KEY, "");
+				final String username = preferences.getString(PREF_USERNAME, "");
 
-                    switch (progress_state)
-                    {
-                        case LOOP:
-                            progress_status++;
-                            break;
-                        case LOGIN:
-                            startActivity(login);
-                            finish();
-                            die = true;
-                            break;
-                        case HOME_TABS:
-                            startActivity(home_tabs);
-                            finish();
-                            die = true;
-                            break;
-                        default:
-                            Log.d(getClass().getCanonicalName(), "Received an unexpected progress state code...");
-                            progress_state = PROGRESS_STATE.LOOP;
-                            break;
-                    }
+				Log.d(getClass().getCanonicalName(), "Key: " + api_key);
+				try {
+					if (api_key.isEmpty()) {
+						// no api key, we need to send them to the login page.
+						Log.d(getClass().getCanonicalName(), "###### API KEY WAS EMPTY");
+						handlePingResult(false);
+						return;
+					} else {
+						// ping the server to check the api_key
+						if (!username.isEmpty()) {
+							// ping server
+							try {
+								Log.d(getClass().getCanonicalName(), "###### PINGING");
+								ApiHandler.GetInstance().doAction(API_ACTION.PING);
+								handlePingResult(true);
+								return;
 
-                    // Update the progress bar
-                    handler.post(new Runnable()
-                    {
-                        public void run()
-                        {
-                            progress.setProgress(progress_status);
-                        }
-                    });
-                }
-            }
-        }).start();
+							} catch (final Exception ex) {
+								Log.d(getClass().getCanonicalName(), "Caught exception. Redirecting to login.");
+								handlePingResult(false);
+								return;
+							}
+						} else {
+							// send them to the login page.
+							Log.d(getClass().getCanonicalName(), "###### NO USERNAME");
+							handlePingResult(false);
+							return;
+						}
+					}
+				} catch (final Exception ex) {
+					Toast.makeText(HuntrMainActivity.this, "WTF LOGIN!?!?!?", Toast.LENGTH_SHORT);
+				}
 
-        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
-        final String api_key = preferences.getString(PREF_API_KEY, "");
-        final String username = preferences.getString(PREF_USERNAME, "");
-        Log.d(getClass().getCanonicalName(), "Key: " + api_key);
-        try
-        {
-            if (api_key.isEmpty())
-            {
-                // no api key, we need to send them to the login page.
-                Log.d(getClass().getCanonicalName(), "###### API KEY WAS EMPTY");
-                progress_state = PROGRESS_STATE.LOGIN;
-            }
-            else
-            {
-                // ping the server to check the api_key
-                if (!username.isEmpty())
-                {
-                    // ping server
-                    try
-                    {
-                        Log.d(getClass().getCanonicalName(), "###### PINGING");
-                        ApiHandler.GetInstance().doAction(API_ACTION.PING);
-                        progress_state = PROGRESS_STATE.HOME_TABS;
+			}
+		}).start();
+	}
 
-                    }
-                    catch (final Exception ex)
-                    {
-                        Log.d(getClass().getCanonicalName(), "Caught exception. Redirecting to login.");
-                        progress_state = PROGRESS_STATE.LOGIN;
-                    }
-                }
-                else
-                {
-                    // send them to the login page.
-                    Log.d(getClass().getCanonicalName(), "###### NO USERNAME");
-                    progress_state = PROGRESS_STATE.LOGIN;
-                }
-            }
-        }
-        catch (final Exception ex)
-        {
-            progress_state = PROGRESS_STATE.LOOP;
-        }
-    }
+	@Override
+	protected void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+
+		setContentView(R.layout.main);
+		progress = (ProgressBar) findViewById(R.id.progress_main);
+		progress.setProgress(50);
+
+	}
 }
