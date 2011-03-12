9e5f9c709d2570f6be30297129a181d524ed9c18
Ben Olive <ben.olive@gatech.edu>
2011-03-12 04:43:52 -0500

    JSON

--- server/app/models/quest.rb
+++ server/app/models/quest.rb
@@ -1,4 +1,5 @@
 class Quest < ActiveRecord::Base
+  self.include_root_in_json = false
   belongs_to :created_by
   has_many :steps
   has_many :user_quests
--- server/app/models/user_quest.rb
+++ server/app/models/user_quest.rb
@@ -1,4 +1,5 @@
 class UserQuest < ActiveRecord::Base
+  self.include_root_in_json = false
   belongs_to :user
   belongs_to :quest
   has_many :user_steps
