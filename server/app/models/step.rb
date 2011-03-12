093344653ff3822a2d7df3c2ccd930c67dcebd8a
Ben Olive <ben.olive@gatech.edu>
2011-03-12 03:23:56 -0500

    Step Validation

--- server/app/models/step.rb
+++ server/app/models/step.rb
@@ -1,4 +1,11 @@
 class Step < ActiveRecord::Base
   belongs_to :quest
   has_many :user_steps
+  
+  validates_presence_of :clue
+  validates_uniqueness_of :clue, scope: 'quest_id'
+  validates_numericality_of :seq, only_integer: true, greater_than: 0
+  [:lat, :long, :error_radius].each do |n|
+    validates_numericality_of n, greater_than: 0
+  end
 end
